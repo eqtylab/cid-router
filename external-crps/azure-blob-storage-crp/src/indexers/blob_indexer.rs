3c37d6aae64d976cd78d89745d49a0b15fe8ab39
cameronfyfe <cameron.j.fyfe@gmail.com>
2024-05-19 18:26:08 -0600

    azure-blob-storage-crp: update blob indexter task loop

--- external-crps/azure-blob-storage-crp/src/indexers/blob_indexer.rs
+++ external-crps/azure-blob-storage-crp/src/indexers/blob_indexer.rs
@@ -5,7 +5,20 @@ use tokio::time::{Duration, Instant};
 
 use crate::{config::IndexingStrategy, context::Context};
 
-pub async fn blob_indexer_task(ctx: Arc<Context>) -> Result<()> {
+pub async fn start(ctx: Arc<Context>) -> Result<()> {
+    let ctx = ctx.clone();
+
+    match blob_indexer_task(ctx).await {
+        Err(e) => {
+            panic!("blob_indexer_task error: {:?}", e);
+        }
+        Ok(()) => {
+            panic!("blob_indexer_task returned, it should never return");
+        }
+    }
+}
+
+async fn blob_indexer_task(ctx: Arc<Context>) -> Result<()> {
     let Context { db, .. } = &*ctx;
 
     match ctx.indexing_strategy {
@@ -15,9 +28,12 @@ pub async fn blob_indexer_task(ctx: Arc<Context>) -> Result<()> {
             loop {
                 let next_update_time = Instant::now() + interval;
 
-                db.update_blob_index(&ctx.blob_storage_config).await?;
-                db.update_blob_index_hashes(&ctx.blob_storage_config)
-                    .await?;
+                if let Err(e) = db.update_blob_index(&ctx.blob_storage_config).await {
+                    log::error!("Error updating blob index: {:?}", e);
+                }
+                if let Err(e) = db.update_blob_index_hashes(&ctx.blob_storage_config).await {
+                    log::error!("Error updating blob index hashes: {:?}", e);
+                }
 
                 if Instant::now() < next_update_time {
                     tokio::time::sleep_until(next_update_time).await;
