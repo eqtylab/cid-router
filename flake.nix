c0bde25d1bc551fc4e2e1042840a05713a4aa218
cameronfyfe <cameron.j.fyfe@gmail.com>
2025-08-26 10:59:57 -0700

    update nix dependencies and rust-toolchain

--- flake.nix
+++ flake.nix
@@ -6,10 +6,7 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs = inputs @ { self, ... }:
